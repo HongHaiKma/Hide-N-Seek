d7cb090c7800cfd968a7975b4a6c98f8a3b15f01
HongHaiKma <nguyenhonghailqd1996@gmail.com>
2021-04-05 07:43:32 +0700

    no message

--- Assets/Game/Scripts/Enemies/Enemy.cs
+++ Assets/Game/Scripts/Enemies/Enemy.cs
@@ -297,14 +297,14 @@ public class Enemy : InGameObject
 
         if (CanSeePlayer() && !IsThroughWall())
         {
-            if (Helper.InRange(tf_Owner.position, m_Char.tf_Owner.position, 1f))
-            {
-                ChangeState(E_CatchState.Instance);
-            }
-            else
-            {
-                m_CatchTime += Time.deltaTime;
-            }
+            // if (Helper.InRange(tf_Owner.position, m_Char.tf_Owner.position, 1f))
+            // {
+            //     ChangeState(E_CatchState.Instance);
+            // }
+            // else
+            // {
+            m_CatchTime += Time.deltaTime;
+            // }
         }
         else
         {
