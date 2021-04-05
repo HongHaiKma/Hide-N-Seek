25887816708493bf75aa0fc24c8d092c5193bcb4
HongHaiKma <nguyenhonghailqd1996@gmail.com>
2021-04-05 18:02:46 +0700

    no message

--- Assets/Game/Scripts/UI/Popups/PopupLevelReward.cs
+++ Assets/Game/Scripts/UI/Popups/PopupLevelReward.cs
@@ -80,7 +80,9 @@ public class PopupLevelReward : UICanvas
 
     public void OnWatchVideoReward()
     {
-        AdsManager.Instance.WatchRewardVideoAds(RewardType.CHARACTER_2);
+        // AdsManager.Instance.WatchRewardVideoAds(RewardType.CHARACTER_2);
+        OnWatchVideoRewardLogic();
+        OnWatchVideoRewardAnim();
     }
 
     public void OnWatchVideoRewardLogic()
