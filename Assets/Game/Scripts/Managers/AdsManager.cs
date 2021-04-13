86ca6ee802d403874485b8ae87900e15ee30dfdc
HongHaiKma <nguyenhonghailqd1996@gmail.com>
2021-04-13 17:57:18 +0700

    no message

--- Assets/Game/Scripts/Managers/AdsManager.cs
+++ Assets/Game/Scripts/Managers/AdsManager.cs
@@ -3,239 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using GoogleMobileAds.Api;
 using System;
-
-// public class AdsManager : Singleton<AdsManager>
-// {
-//     public static string Android_Key = "bc33f32d";
-//     private string m_ApplicationKey;
-
-//     public bool openRwdAds;
-//     private RewardType m_RewardType;
-
-//     public bool m_WatchInter;
-
-//     private void Awake()
-//     {
-// #if UNITY_ANDROID
-//         m_ApplicationKey = Android_Key;
-// #elif UNITY_IPHONE
-//         // m_ApplicationKey = IOS_Key;
-// #endif
-
-//         IronSourceConfig.Instance.setClientSideCallbacks(true);
-//         string id = IronSource.Agent.getAdvertiserId();
-//         IronSource.Agent.validateIntegration();
-
-//         IronSource.Agent.init(m_ApplicationKey);
-
-//         IronSourceEvents.onRewardedVideoAdOpenedEvent += RewardedVideoAdOpenedEvent;
-//         IronSourceEvents.onRewardedVideoAdClosedEvent += RewardedVideoAdClosedEvent;
-//         // IronSourceEvents.onRewardedVideoAvailabilityChangedEvent += RewardedVideoAvailabilityChangedEvent;
-//         IronSourceEvents.onRewardedVideoAdStartedEvent += RewardedVideoAdStartedEvent;
-//         IronSourceEvents.onRewardedVideoAdEndedEvent += RewardedVideoAdEndedEvent;
-//         IronSourceEvents.onRewardedVideoAdRewardedEvent += RewardedVideoAdRewardedEvent;
-//         IronSourceEvents.onRewardedVideoAdShowFailedEvent += RewardedVideoAdShowFailedEvent;
-
-
-//         IronSourceEvents.onInterstitialAdLoadFailedEvent += InterstitialAdLoadFailedEvent;
-//         IronSourceEvents.onInterstitialAdShowSucceededEvent += InterstitialAdShowSucceededEvent;
-//         IronSourceEvents.onInterstitialAdShowFailedEvent += InterstitialAdShowFailedEvent;
-//         IronSourceEvents.onInterstitialAdClickedEvent += InterstitialAdClickedEvent;
-//         IronSourceEvents.onInterstitialAdOpenedEvent += InterstitialAdOpenedEvent;
-//         IronSourceEvents.onInterstitialAdClosedEvent += InterstitialAdClosedEvent;
-
-//         IronSource.Agent.loadInterstitial();
-//     }
-
-//     private void OnEnable()
-//     {
-//         // EventManager.AddListener(GameEvent.CHAR_WIN, WatchInterstitial);
-//     }
-
-//     private void OnDisable()
-//     {
-//         // EventManager.RemoveListener(GameEvent.CHAR_WIN, WatchInterstitial);
-//     }
-
-//     void RewardedVideoAdOpenedEvent()
-//     {
-
-//     }
-
-//     #region VIDEO
-
-//     void RewardedVideoAdClosedEvent()
-//     {
-//         if (!openRwdAds)
-//         {
-//             switch (m_RewardType)
-//             {
-//                 case RewardType.GOLD_1:
-//                     EventManager.CallEvent(GameEvent.ADS_GOLD_1_ANIM);
-//                     break;
-//                 case RewardType.GOLD_2:
-//                     EventManager.CallEvent(GameEvent.ADS_GOLD_2_ANIM);
-//                     break;
-//                 case RewardType.CHARACTER_2:
-//                     EventManager.CallEvent(GameEvent.ADS_CHARACTER_2_ANIM);
-//                     break;
-//             }
-//         }
-//     }
-
-//     void RewardedVideoAvailabilityChangedEvent(bool available)
-//     {
-//         bool rewardedVideoAvailability = available;
-//     }
-
-//     void RewardedVideoAdStartedEvent()
-//     {
-
-//     }
-
-//     void RewardedVideoAdEndedEvent()
-//     {
-//         if (openRwdAds)
-//         {
-//             openRwdAds = false;
-
-//             switch (m_RewardType)
-//             {
-//                 case RewardType.CHARACTER:
-//                     EventManager.CallEvent(GameEvent.ADS_CHARACTER_LOGIC);
-//                     break;
-//                 case RewardType.CHARACTER_2:
-//                     m_WatchInter = false;
-//                     EventManager.CallEvent(GameEvent.ADS_CHARACTER_2_LOGIC);
-//                     break;
-//                 case RewardType.GOLD_1:
-//                     EventManager.CallEvent(GameEvent.ADS_GOLD_1_LOGIC);
-//                     break;
-//                 case RewardType.GOLD_2:
-//                     m_WatchInter = false;
-//                     EventManager.CallEvent(GameEvent.ADS_GOLD_2_LOGIC);
-//                     break;
-//             }
-//         }
-//     }
-
-//     void RewardedVideoAdRewardedEvent(IronSourcePlacement placement)
-//     {
-//         if (openRwdAds)
-//         {
-//             openRwdAds = false;
-
-//             switch (m_RewardType)
-//             {
-//                 case RewardType.CHARACTER:
-//                     EventManager.CallEvent(GameEvent.ADS_CHARACTER_LOGIC);
-//                     break;
-//                 case RewardType.CHARACTER_2:
-//                     m_WatchInter = false;
-//                     EventManager.CallEvent(GameEvent.ADS_CHARACTER_2_LOGIC);
-//                     break;
-//                 case RewardType.GOLD_1:
-//                     EventManager.CallEvent(GameEvent.ADS_GOLD_1_LOGIC);
-//                     break;
-//                 case RewardType.GOLD_2:
-//                     m_WatchInter = false;
-//                     EventManager.CallEvent(GameEvent.ADS_GOLD_2_LOGIC);
-//                     break;
-//             }
-//         }
-//     }
-
-//     void RewardedVideoAdShowFailedEvent(IronSourceError error)
-//     {
-
-//     }
-
-//     public void WatchRewardVideoAds(RewardType _rewardType)
-//     {
-//         m_RewardType = _rewardType;
-//         openRwdAds = true;
-//         bool available = IronSource.Agent.isRewardedVideoAvailable();
-
-//         if (available)
-//         {
-//             IronSource.Agent.showRewardedVideo();
-//         }
-//     }
-
-//     #endregion
-
-//     #region INTER
-
-//     void InterstitialAdLoadFailedEvent(IronSourceError error)
-//     {
-//         IronSource.Agent.loadInterstitial();
-//     }
-//     //Invoked right before the Interstitial screen is about to open.
-//     void InterstitialAdShowSucceededEvent()
-//     {
-//         IronSource.Agent.loadInterstitial();
-//     }
-//     //Invoked when the ad fails to show.
-//     //@param description - string - contains information about the failure.
-//     void InterstitialAdShowFailedEvent(IronSourceError error)
-//     {
-//         IronSource.Agent.loadInterstitial();
-//     }
-//     // Invoked when end user clicked on the interstitial ad
-//     void InterstitialAdClickedEvent()
-//     {
-
-//     }
-//     //Invoked when the interstitial ad closed and the user goes back to the application screen.
-//     void InterstitialAdClosedEvent()
-//     {
-//         IronSource.Agent.loadInterstitial();
-//     }
-//     //Invoked when the Interstitial is Ready to shown after load function is called
-//     void InterstitialAdReadyEvent()
-//     {
-
-//     }
-//     //Invoked when the Interstitial Ad Unit has opened
-//     void InterstitialAdOpenedEvent()
-//     {
-
-//     }
-
-//     public void WatchInterstitial()
-//     {
-//         if (m_WatchInter)
-//         {
-//             if (IronSource.Agent.isInterstitialReady())
-//             {
-//                 IronSource.Agent.showInterstitial();
-//             }
-//         }
-//     }
-
-//     #endregion
-// }
-
-// public enum RewardType
-// {
-//     CHARACTER,
-//     CHARACTER_2,
-//     GOLD_1,
-//     GOLD_2,
-// }
+using GoogleMobileAdsMediationTestSuite.Api;
 
 public class AdsManager : Singleton<AdsManager>
 {
+    private string m_APP_ID = "ca-app-pub-8721698442392956~1232205283";
+
     private BannerView m_BannerView;
-    private string m_BannerId = "ca-app-pub-8721698442392956/1702569451";
+    private string m_BannerId = "ca-app-pub-8721698442392956/8871323474";
     public bool m_BannerLoaded;
 
     private InterstitialAd interstitial;
-    private string m_InterId = "ca-app-pub-8721698442392956/3416952287";
+    private string m_InterId = "ca-app-pub-8721698442392956/5034707168";
     public bool m_WatchInter;
 
     private RewardedAd rewardedAd;
-    private string m_RewardId = "ca-app-pub-8721698442392956/4408623845";
+    private string m_RewardId = "ca-app-pub-8721698442392956/2114343439";
 
 
     public bool openRwdAds;
@@ -248,6 +31,7 @@ public class AdsManager : Singleton<AdsManager>
         m_WatchInter = true;
 
         MobileAds.Initialize(initStatus => { });
+        // MobileAds.Initialize(m_APP_ID);
 
         this.RequestBanner();
         this.RequestInter();
@@ -342,17 +126,11 @@ public class AdsManager : Singleton<AdsManager>
     {
         this.rewardedAd = new RewardedAd(m_RewardId);
 
-        // Called when an ad request has successfully loaded.
         this.rewardedAd.OnAdLoaded += HandleRewardedAdLoaded;
-        // Called when an ad request failed to load.
         this.rewardedAd.OnAdFailedToLoad += HandleRewardedAdFailedToLoad;
-        // Called when an ad is shown.
-        this.rewardedAd.OnAdOpening += HandleRewardedAdOpening;
-        // Called when an ad request failed to show.
+        // this.rewardedAd.OnAdOpening += HandleRewardedAdOpening;
         this.rewardedAd.OnAdFailedToShow += HandleRewardedAdFailedToShow;
-        // Called when the user should be rewarded for interacting with the ad.
         this.rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
-        // Called when the ad is closed.
         this.rewardedAd.OnAdClosed += HandleRewardedAdClosed;
 
         // Create an empty ad request.
@@ -377,7 +155,13 @@ public class AdsManager : Singleton<AdsManager>
 
     public void LoadRewardVideo()
     {
-        // this.rewardedAd = new RewardedAd(m_RewardId);
+        this.rewardedAd = new RewardedAd(m_RewardId);
+        this.rewardedAd.OnAdLoaded += HandleRewardedAdLoaded;
+        this.rewardedAd.OnAdFailedToLoad += HandleRewardedAdFailedToLoad;
+        // this.rewardedAd.OnAdOpening += HandleRewardedAdOpening;
+        this.rewardedAd.OnAdFailedToShow += HandleRewardedAdFailedToShow;
+        this.rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
+        this.rewardedAd.OnAdClosed += HandleRewardedAdClosed;
         AdRequest request = new AdRequest.Builder().Build();
         this.rewardedAd.LoadAd(request);
     }
@@ -456,7 +240,7 @@ public class AdsManager : Singleton<AdsManager>
             "HandleRewardedAdFailedToShow event received with message: "
                              + args.Message);
 
-        // LoadRewardVideo();
+        LoadRewardVideo();
     }
 
     public void HandleRewardedAdClosed(object sender, EventArgs args)
@@ -527,16 +311,22 @@ public class AdsManager : Singleton<AdsManager>
 
     public void WatchRewardVideo(RewardType _rewardType)
     {
-        LoadRewardVideo();
+        StartCoroutine(IEWatchRewardVideo(_rewardType));
+    }
+
+    IEnumerator IEWatchRewardVideo(RewardType _rewardType)
+    {
+        // LoadRewardVideo();
 
         m_RewardType = _rewardType;
         openRwdAds = true;
-        // bool available = IronSource.Agent.isRewardedVideoAvailable();
 
-        // if (available)
-        // {
-        //     IronSource.Agent.showRewardedVideo();
-        // }
+        GUIManager.Instance.GetPanelLoadingAds().g_Content.SetActive(true);
+
+        yield return Yielders.Get(1f);
+        yield return Yielders.EndOfFrame;
+
+        GUIManager.Instance.GetPanelLoadingAds().g_Content.SetActive(false);
 
         if (this.rewardedAd.IsLoaded())
         {
@@ -545,7 +335,15 @@ public class AdsManager : Singleton<AdsManager>
         else
         {
             LoadRewardVideo();
+            GUIManager.Instance.GetPanelLoadingAds().g_NetworkError.SetActive(true);
+            yield return Yielders.Get(0.5f);
+            GUIManager.Instance.GetPanelLoadingAds().g_NetworkError.SetActive(false);
         }
+    }
+
+    public void ShowMediationTestSuite()
+    {
+        MediationTestSuite.Show();
     }
 }
 
--- Assets/Game/Scripts/PanelLoadingAds.cs
+++ Assets/Game/Scripts/PanelLoadingAds.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class PanelLoadingAds : MonoBehaviour
 {
     public GameObject g_Content;
+    public GameObject g_NetworkError;
 }
